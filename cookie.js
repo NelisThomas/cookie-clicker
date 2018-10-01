c919d38f98eb8a9396bf03d48793f80d5de288c2
Thomas Nelis <tomnelis53@gmail.com>
2018-10-01 11:25:51 +0200

    help my laptop is dying

--- cookie.js
+++ cookie.js
@@ -10,18 +10,40 @@ function closeNav() {
 var count = 0;
 var multiplier = 1;
 /*COUNTER INCREMENTATION FUNCTION*/
-function counterIncrement() {
+    function counterIncrement() {
     count= count +(1*multiplier);
     document.getElementById("counter").innerHTML = "$ "+count;
+    document.getElementById("multiplier").innerHTML = "x"+multiplier;
 }
 /*UPGRADES*/
+
+
+var price1 = 10;
 function extraClick() {
-    if (count > 9) {
-        count = count - 10;
+    if (count >= price1) {
+        count = count - price1;
         multiplier = multiplier + 1;
+        price1 = price1*2;
         document.getElementById("counter").innerHTML = "$ " + count;
+        document.getElementById("multiplier").innerHTML = "x"+multiplier;
+        document.getElementById("doubleClickButton").value = "$"+price1;
     }
     else {
         alert("Not enough $");
     }
+}
+
+var price2 = 5;
+var toggleAutoClick = 0;
+function autoClicker(){
+    if (count >= price2){
+        count = count - price2;
+        toggleAutoClick = 1;
+    }
+    else {
+        alert("Not enough $");
+    }
+}
+while (toggleAutoClick=1){
+    setInterval(counterIncrement, 1000);
 }
